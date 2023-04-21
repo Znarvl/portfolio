7f908cfc7e380e18bb263de046e372d290d4d26f
Simon <simme.jakobsson@gmail.com>
2023-04-21 11:27:35 +0200

    added timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import timeline from "../data/timeline";
+import TimelineItem from "./TimelineItem";
 
 function Timeline(){
 
     return (
-        <div>Timeline</div>
+        <div className="flex flex-col md:flex-row justify-center my-20 w-full">
+            <div className="w-full md:w-7/12">
+            {timeline.map(item =>(
+                <TimelineItem
+                    year= {item.year}
+                    title = {item.title}
+                    duration = {item.duration}
+                    details = {item.details} />
+
+                ))}
+            </div>
+        </div>
     )
 }
 
---
+++ src/data/timeline.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        year: '2023',
+        title: 'Masters in software engineering and computer science',
+        duration: '2 years',
+        details: 'dorem ipsum'
+    },
+    {
+        year: '2021',
+        title: "Bachelor's computer engineering",
+        duration: '3 years',
+        details: ''
+    },
+]
