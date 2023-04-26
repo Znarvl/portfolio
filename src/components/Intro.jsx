ef0052d66a4059b4a8692150ca8fde421c89e1ee
Simon <simme.jakobsson@gmail.com>
2023-04-26 22:10:36 +0200

    started reworing intro page, added some fancy animations and background

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import SouthIcon from '@mui/icons-material/South';
 
 function Intro(){
 
     return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Simon Jakobsson</h1>
-            <p className="text-md max-w-xl mb-6 font-bold">My fine bio</p>
-
+        <div className="min-h-screen flex items-center justify-center flex-col text-center pt-20 pb-6 bg-gradient-to-r 
+        from-purple-500
+        via-orange-500
+        to-blue-500
+        background-animate">
+            <h1 className="text-4xl md:text-7xl text-white mb-1 md:mb-3 font-bold">Hello!</h1>
+            <p className="text-md text-white max-w-xl mb-6 font-bold">My name is Simon and I'm studying </p>
+        <div class="animate-bounce w-6 h-6 text-white absolute bottom-7 ">
+            <SouthIcon fontSize="large"/>
+        </div>
         </div>
     )
 }
