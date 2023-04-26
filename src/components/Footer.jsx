d9c292b2d11aa8c6511913a62b84ece82b6b117d
Simon <simme.jakobsson@gmail.com>
2023-04-26 21:06:20 +0200

    added contact, footer and title to all of them

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Footer(){
 
     return (
-        <div></div>
+        <div className="py-5 text-center">
+            <p className="text-sm mt-2 opacity-50">
+               &copy; {new Date().getFullYear()} Simon Jakobsson. All rights reserved
+            </p>
+
+        </div>
     )
 }
 
