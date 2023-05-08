ce01e6d4b5ce7b51ad505c9d14571a9abe0ec8f9
Simon <simme.jakobsson@gmail.com>
2023-05-08 22:24:07 +0200

    added title to portfolio, need to be align correctly

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import portfolio from "../data/portfolio";
 import PortfolioItem from "./PortfolioItem";
+import Title from "./Title";
+
 
 function Portfolio(){
 
     return (
-        <div id='portfolio' className="flex flex-col md:flex-row items-center justify-center ">
+        <div>
+            <div id='portfolio' className="justify-center ml-25">
+            <Title >Portfolio</Title>
+            </div>
+        <div  className="flex flex-col md:flex-row items-center justify-center ">
+            
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-20">
                 {portfolio.map(project =>(
                     <PortfolioItem
@@ -16,7 +23,7 @@ function Portfolio(){
                     />
                 ))}
             </div>
-
+            </div>
         </div>
     )
 }
