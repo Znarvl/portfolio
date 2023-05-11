801cd723a4626980b2d2ed247409d19a855ac96a
Simon <simme.jakobsson@gmail.com>
2023-05-11 22:11:51 +0200

    check if actions works

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -21,7 +21,7 @@ function Intro(){
             <h1 className="text-4xl md:text-7xl text-white mb-1 md:mb-3 font-bold">Hello!</h1>
             <p className="text-md text-white max-w-xl mb-6 font-bold">My name is Simon and I'm pursuing a degree in Software Engineering and Computer Science at Linköpning University. 
             I'm enthusiastic about music, web development and artificial intelligence/Machine learning.
-             In my free time, I'm tinkering with web development and small machine learning project, or playing music.   </p>
+             In my free time, I'm tinkering with web development and small machine learning project, or playing music.  </p>
              <div className="text-white flex space-x-4 mt-20">
             <LinkedInIcon onClick={event =>  window.location.href='https://www.linkedin.com/in/simon-jakobsson-871252144/'} fontSize="large"/>
             <GitHubIcon onClick={event =>  window.location.href='https://github.com/Znarvl'} fontSize="large"/>
