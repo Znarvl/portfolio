46df9c1435422036c2402059d46e39ea5f8cfb86
Simon Jakobsson <simonjakobsson@Simons-MacBook-Pro.local>
2023-06-12 21:40:36 +0200

    checking if it works again

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
     {
         title: 'Portfolio',
-        imgUrl: '/assets/portfolio.png',
+        imgUrl: '/portfolio/assets/portfolio.png',
         stack: ['JavaScript', 'TailwindCSS', 'React'],
         link: 'https://github.com/Znarvl/portfolio'
     },
     {
         title: 'Fake news classification',
-        imgUrl: '/assets/fake_news.jpeg',
+        imgUrl: '/portfolio/assets/fake_news.jpeg',
         text: 'A university course in text mining I identified fake news using Google BERT ',
         stack: ['Python', 'Pandas', 'BERT','TensorFlow',],
         link: 'https://github.com/Znarvl/TDDE16_project'
     },
     {
         title: 'University courses',
-        imgUrl: '/assets/uni_courses.png',
+        imgUrl: '/portfolio/assets/uni_courses.png',
         stack: ['C/C++', 'Python', 'Java','R', 'And much more'],
         link: 'https://github.com/Znarvl/TDDE16_project'
     }
