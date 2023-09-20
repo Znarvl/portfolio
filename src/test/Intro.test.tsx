b1f491aba7445cf22a638e8bfcde52748f06c4a8
Simon Jakobsson <simme.jakobsson@gmail.com>
2023-09-20 18:27:06 +0200

    intentionally added faulty code

--- src/test/Intro.test.tsx
+++ src/test/Intro.test.tsx
@@ -5,6 +5,6 @@ import Navigation from '../components/Navigation'
 describe('App', () => {
   it('Vite to be in document', () => {
     render(<Navigation />)
-    expect(screen.getByText('Projects')).toBeInTheDocument()
+    expect(screen.getByText('poop')).toBeInTheDocument()
   })
 })
