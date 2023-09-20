1c2d6055d26d8dbe5a0b0e2a58825f749106206b
Simon Jakobsson (External) <simon.jakobsson@knowit.se>
2023-09-20 12:07:19 +0200

    changed deployment to build and test before deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -7,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [react({ fastRefresh: false })],
+  plugins: [react({ fastRefresh: false })], // If not fastRefresh is false, testing wont work, a TODO is to find a better workaround
   worker: {
     plugins: [react()],
   },
