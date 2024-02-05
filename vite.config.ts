59d2e6f2051b9506a59b65465166dcc997e54614
Simon Jakobsson <simme.jakobsson@gmail.com>
2024-02-05 19:18:02 +0100

    updated to vite 5.x

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [react({ fastRefresh: false })], // If not fastRefresh is false, testing wont work, a TODO is to find a better workaround
-  worker: {
-    plugins: [react()],
-  },
+  plugins: [react()], // If not fastRefresh is false, testing wont work, a TODO is to find a better workaround
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
     css:true
    },
+   
 })
