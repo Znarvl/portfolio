89df0588660778b8bbc776f40d54c6146e943b15
Simon Jakobsson <simme.jakobsson@gmail.com>
2025-02-21 10:03:40 +0100

    Updated timeline with new job

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -2,10 +2,17 @@ import type ITimeline from '../interfaces/ITimeline'
 
 const timeline: ITimeline[] = [
   {
+    year: 'Mars 2025',
+    title: 'Software Engineer',
+    duration: 'Ongoing',
+    details: 'Fullstack development with focus on the Android platform',
+    at: 'CPAC Systems'
+  },
+  {
     year: 'Mars 2024',
     title: 'Software Developer',
-    duration: 'Ongoing',
-    details: 'C++ Adaptive AUTOSAR development with a focus on the Transport Layer and JSON parsing',
+    duration: 'Mars 2025',
+    details: 'C++ Adaptive AUTOSAR development with focus on DoIP',
     at: 'Vector Sweden'
   },
   {
